6f5a5f12b47e5c144255863ce3e0b83f849ba92d
Alex Trilling <atrill90@gmail.com>
2023-03-06 21:48:02 -0500

    header text added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="gpt3__header section__padding" id="home">
+            <div className="gpt3__header-content">
+                <h1 className="gradient__text">
+                Let’s Build Something amazing with GPT-3 OpenAI
+                </h1>
+                <div className="gpt3__header-content__input">
+                    <input type="email" placeholder="Your Email Address"/>
+                    <button type="button">Get Started</button>
+                </div>
+            </div>
         </div>
     )
 }
